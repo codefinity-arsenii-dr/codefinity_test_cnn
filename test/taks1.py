47716f02697340a196ac19ac50b62de920af864a
codefinity-arsenii-dr <arsenii.drobotenko@codefinity.com>
2025-03-25 14:33:20 +0200

    create test for preprocessing

---
+++ test/taks1.py
@@ -0,0 +1,28 @@
+import numpy as np
+from utils import display_hint, display_solution, display_check
+
+def hint1():
+    hint = """
+Ensure that pixel values are scaled to [0,1] by dividing by 255.0.
+Also, use `keras.utils.to_categorical` to convert labels into one-hot encoded vectors.
+"""
+    display_hint(hint)
+
+def solution1():
+    code = """
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+"""
+    display_solution(code)
+
+def check1(x_train, x_test, y_train, y_test):
+    if not (np.max(x_train) <= 1.0 and np.min(x_train) >= 0.0):
+        display_check(False, "x_train is not properly normalized.")
+    elif not (np.max(x_test) <= 1.0 and np.min(x_test) >= 0.0):
+        display_check(False, "x_test is not properly normalized.")
+    elif y_train.shape[1] != 10 or y_test.shape[1] != 10:
+        display_check(False, "Labels are not correctly one-hot encoded.")
+    else:
+        display_check(True, "Correct! Here is the next part of the key: XYZ123")
---
+++ utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+from IPython.display import display_markdown, display_html
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+
+
+def display_hint(text: str):
+    display_markdown(text, raw=True)
+
+
+def display_solution(code: str):
+    lexer = get_lexer_by_name("python", stripall=True)
+    formatter = HtmlFormatter(style="monokai")
+    style = f"""<style>{formatter.get_style_defs(".output_html")}</style>"""
+    display_html(style + highlight(code, lexer, formatter), raw=True)
+
+
+def display_check(correct: bool, text: str):
+    display_html(
+        f"""<span style="color: {"green" if correct else "red"}">{text}</span>""",
+        raw=True,
+    )
+
+
+def np_random_generator(fixed_rng: bool) -> np.random.Generator:
+    if fixed_rng:
+        return np.random.default_rng(seed=42)
+    else:
+        return np.random.default_rng()
